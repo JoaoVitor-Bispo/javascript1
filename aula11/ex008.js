f4ddbccdeeec803abbff587327ed1b61beeb372f
GabrielJunio <gabrieljn080@gmail.com>
2022-08-18 15:53:30 -0300

    Iniciando em Condicionais

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var velocidade = 65
+if (velocidade > 60) {
+    console.log('Voce ultrapassou o limite de velocidade. Multado!')
+}
+console.log('Prossiga em segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Eua'
+if (país != 'Brasil') {
+    console.log('Você é estrangeiro')
+}
+else {
+    console.log('Você é brasileiro')
+}
