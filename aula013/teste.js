f06836a6a69cc87620f23f8fffb9d25495cf9826
GabrielJunio <gabrieljn080@gmail.com>
2022-08-24 16:40:23 -0300

    finalizando exercícios sobre condicionais

---
+++ aula013/teste.js
@@ -0,0 +1,5 @@
+c = 0
+do {
+    c++
+    console.log(c)
+} while(c != 5)
--- aula12ex/ex016/script.js
+++ aula12ex/ex016/script.js
@@ -9,12 +9,39 @@ function verificar() {
     else {
         fsexo = window.document.getElementsByName('sexo')
         idade = ano - Number(nascimento.value)
+        var imagem = document.createElement('img')
         if (fsexo[0].checked) {
              var genero = 'Homem'
+             if (idade >= 0 && idade < 15) {
+               imagem.setAttribute('src', 'imagens/foto-bebe-m.png')
+             }
+             else if (idade < 26) {
+                imagem.setAttribute('src', 'imagens/foto-jovem-m.png')
+             }
+             else if (idade >= 26 && idade < 60) {
+                imagem.setAttribute('src', 'imagens/foto-adulto-m.png')
+             }
+             else {
+                imagem.setAttribute('src', 'imagens/foto-idoso-m.png')
+             }
         }
         else {
             var genero = 'Mulher'
+            if (idade >= 0 && idade < 15) {
+                imagem.setAttribute('src', 'imagens/foto-bebe-f.png')
+              }
+              else if (idade < 26) {
+                 imagem.setAttribute('src', 'imagens/foto-jovem-f.png')
+              }
+              else if (idade >= 26 && idade < 60) {
+                 imagem.setAttribute('src', 'imagens/foto-adulto-f.png')
+              }
+              else {
+                 imagem.setAttribute('src', 'imagens/foto-idoso-f.png')
+              }
         }
-        res.innerHTML = `${genero}`
+        res.style.textAlign = 'center'
+        res.innerHTML = `${genero} com ${idade}`
+        res.appendChild(imagem)
     }
 }
