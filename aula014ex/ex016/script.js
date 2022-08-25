a5aa7894711d45567fb0eb4418698a466c8e48ff
GabrielJunio <gabrieljn080@gmail.com>
2022-08-25 15:00:56 -0300

    estruturas de repetição

---
+++ aula014ex/ex016/script.js
@@ -0,0 +1,8 @@
+function contagem() {
+    var inicio = window.document.getElementById('começo')
+    var fim = window.document.getElementById('final')
+    var passo = window.document.getElementById('pulo')
+    var res = window.document.getElementById('res')
+    for (var c = Number(inicio.value);c <= Number(fim.value); c+= Number(passo.value)) {
+        
+}
