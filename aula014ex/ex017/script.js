d0b128ca8d7b022eabd0620ef5591adb351f9b6f
GabrielJunio <gabrieljn080@gmail.com>
2022-08-29 14:49:32 -0300

    finalizando exercícios sobre estrutura de repetição

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -1,2 +1,16 @@
-var cor_bg = window.document.getElementById('cria')
-
+function gerar() {
+    var num1 = window.document.getElementById('numb')
+    var tabu = window.document.getElementById('tab')
+    if (num1.lenght == 0) {
+        window.alert('Por favor, digite um número.')
+    }
+    else {
+        var num = Number(num1.value)
+        tabu.innerHTML = ''
+        for (var c = 0;c <= 10;c++) {
+            var item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num * c}`
+            tabu.appendChild(item)
+        }    
+    }
+}
