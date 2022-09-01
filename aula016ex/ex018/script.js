42f66306ca4cdcef3e5998ca558b72af1744ddc8
GabrielJunio <gabrieljn080@gmail.com>
2022-09-01 15:09:53 -0300

    começando ultimo exercício

--- aula016ex/ex018/script.js
+++ aula016ex/ex018/script.js
@@ -2,20 +2,36 @@ var array = []
 var n1 = window.document.getElementById('num')
 var result = window.document.getElementById('ressl')
 var res = window.document.getElementById('res')
+function final() {
+    res.innerHTML = `${maior_num}`
+}
 function adicionar() {
-    while (true) {
         if (n1.value.length == 0) {
             window.alert('[ERRO] Por favor digite algum número.')
         }
         else {
             n = Number(n1.value)
-            array.push(n)
-            var elemento = document.createElement('option')
-            elemento.text = `O número ${n} foi adicionado`
-            result.appendChild(elemento)
-            break
-        }
-    res.innerHTML = `${array}`
+            if (array.indexOf(n) != -1) {
+                window.alert('Esse valor já foi digitado.')
+            }
+            else {
+                res.innerText = ''
+                array.push(n)
+                var elemento = document.createElement('option')
+                elemento.text = `O número ${n} foi adicionado`
+                result.appendChild(elemento)
+                for (num_c in array) {
+                    if (array[num_c] > array[num_c]) {
+                        maior_num = array[num_c]
+                    }
+                    else if (array[num_c] < array[num_c - 1]) {
+                        menor_num = array[num_c]
+                    }
+
+
+                }
+            }
     }
 }
+
 
---
+++ test.js
@@ -0,0 +1,5 @@
+var array = [1,5,2,3,1]
+for (var num_c1 in array) {
+}
+console.log(num_c1)
+
