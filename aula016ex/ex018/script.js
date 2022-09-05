e3484926dec0842dcfe2d5658077d5df345ea94c
GabrielJunio <gabrieljn080@gmail.com>
2022-09-05 15:07:56 -0300

    ultimo ex

--- aula016ex/ex018/script.js
+++ aula016ex/ex018/script.js
@@ -2,12 +2,33 @@ var array = []
 var n1 = window.document.getElementById('num')
 var result = window.document.getElementById('ressl')
 var res = window.document.getElementById('res')
+var total_array = 0
+var maior_num = 0
 function final() {
-    res.innerHTML = `${maior_num}`
+    for (var num_c in array) {
+        var num = array[num_c]
+        if (num_c > 0) {
+            maior_num = array[num_c]
+            if (maior_num > num) {
+                maior_num = maior_num
+            }
+            else {
+                maior_num = num
+            }
+
+            }
+        total_array = total_array + array[num_c]
+        }   
+    res.innerHTML = `Temos ${array.length} números cadastrados ao todo`
+    res.innerHTML += `A soma dos números do array é ${total_array}`   
+    res.innerHTML += `O maior número do array é ${maior_num}` 
 }
 function adicionar() {
         if (n1.value.length == 0) {
             window.alert('[ERRO] Por favor digite algum número.')
+        }
+        else if (n1.value > 100) {
+            window.alert('[ERRO] Digite números entre 1 à 100.')
         }
         else {
             n = Number(n1.value)
@@ -20,16 +41,6 @@ function adicionar() {
                 var elemento = document.createElement('option')
                 elemento.text = `O número ${n} foi adicionado`
                 result.appendChild(elemento)
-                for (num_c in array) {
-                    if (array[num_c] > array[num_c]) {
-                        maior_num = array[num_c]
-                    }
-                    else if (array[num_c] < array[num_c - 1]) {
-                        menor_num = array[num_c]
-                    }
-
-
-                }
             }
     }
 }
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-var array = [1,5,2,3,1]
-for (var num_c1 in array) {
-}
-console.log(num_c1)
-
+array = [1,2,3,4,5]
+var total_array = 0
+for (var num_c in array) {
+    total_array = total_array + array[num_c]
+ }
+console.log(total_array)
