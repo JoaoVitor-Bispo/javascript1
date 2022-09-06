7d90964faaba9c38dfdc8fb5b06b5827767892c3
GabrielJunio <gabrieljn080@gmail.com>
2022-09-06 15:18:39 -0300

    finalizando ultimo exercício

--- aula016ex/ex018/script.js
+++ aula016ex/ex018/script.js
@@ -2,33 +2,45 @@ var array = []
 var n1 = window.document.getElementById('num')
 var result = window.document.getElementById('ressl')
 var res = window.document.getElementById('res')
-var total_array = 0
-var maior_num = 0
+
 function final() {
-    for (var num_c in array) {
-        var num = array[num_c]
-        if (num_c > 0) {
-            maior_num = array[num_c]
-            if (maior_num > num) {
-                maior_num = maior_num
-            }
+    if (array.length == 0) {
+        window.alert('[ERRO] Digite algum número antes de finalizar.')
+    }
+    else {
+        var maior_num = 0
+        var menor_num = 0
+        var total_array = 0
+        for (var num_c in array) {
+            var num = array[num_c]
+            if (num_c > 0) {
+                if (maior_num < num) {
+                    maior_num = num
+                }   
+                if (menor_num > num) {
+                    menor_num = num
+                }  
+                }
             else {
                 maior_num = num
+                menor_num = num
             }
-
-            }
-        total_array = total_array + array[num_c]
-        }   
-    res.innerHTML = `Temos ${array.length} números cadastrados ao todo`
-    res.innerHTML += `A soma dos números do array é ${total_array}`   
-    res.innerHTML += `O maior número do array é ${maior_num}` 
+            total_array = total_array + array[num_c]
+            var media = total_array / array.length
+        }
+    res.innerHTML = `Temos ${array.length} números cadastrados ao todo.<br>`
+    res.innerHTML += `A soma dos números do array é ${total_array}<br>`   
+    res.innerHTML += `O maior número do array é ${maior_num}<br>`
+    res.innerHTML += `O menor número do array é ${menor_num}<br>`
+    res.innerHTML += `A média entre os números do array é de ${media}`
+    }
 }
 function adicionar() {
         if (n1.value.length == 0) {
             window.alert('[ERRO] Por favor digite algum número.')
         }
-        else if (n1.value > 100) {
-            window.alert('[ERRO] Digite números entre 1 à 100.')
+        else if (n1.value > 100 || n1.value < 1) {
+            window.alert('[ERRO] Digite apenas números que estejam entre 1 à 100.')
         }
         else {
             n = Number(n1.value)
@@ -42,7 +54,7 @@ function adicionar() {
                 elemento.text = `O número ${n} foi adicionado`
                 result.appendChild(elemento)
             }
-    }
+        }
 }
 
 
--- test.js
+++ test.js
@@ -1,6 +1,2 @@
-array = [1,2,3,4,5]
-var total_array = 0
-for (var num_c in array) {
-    total_array = total_array + array[num_c]
- }
-console.log(total_array)
+array = []
+console.log(array)
