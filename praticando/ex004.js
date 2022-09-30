a8d431dd56989badcba4ee567ee6a53a792d75c4
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2022-09-30 19:46:52 -0300

    att

--- praticando/ex004.js
+++ praticando/ex004.js
@@ -1,6 +1,6 @@
 const prompt = require("prompt-sync")();
 
-var tabuada = prompt('Digite um número para a tabuada: ')
+var tabuada = 5
 for (var c = 0; c < 11; c++) {
     console.log(`${c} x ${tabuada} = ${c * tabuada}`)
 }
