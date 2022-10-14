97bc95569d80d3bc1433f03726d24c7842c7b764
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2022-10-14 17:37:38 -0300

    at

--- praticando/ex008.js
+++ praticando/ex008.js
@@ -1,2 +1,19 @@
-let num = 5;
-let num = 3;
+let array = [12,3,5,3,4,7,8,10,14,16,2,1,3,5,7]
+let pares = []
+let impares = []
+let c = 0
+while (true) {
+    if (array[c] % 2 == 0) {
+        pares.push(array[c])
+    }
+    else {
+        impares.push(array[c])
+    }
+    c += 1
+    if (c == array.length) {
+        break
+    }
+}
+console.log(array)
+console.log(pares)
+console.log(impares)
---
+++ praticando/ex009.js
@@ -0,0 +1,7 @@
+function soma(a=0,b=0) {
+    parseFloat(a)
+    parseFloat(b)
+    console.log(`Um terreno de ${a}x${b} tem `)
+    return a * b
+}
+console.log(soma(5.3,3))
