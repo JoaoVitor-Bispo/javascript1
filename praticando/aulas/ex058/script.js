9941fded37953cc33e0e2e380b18fd1763135bb4
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2023-03-21 19:33:43 -0300

    API project

---
+++ praticando/aulas/ex058/script.js
@@ -0,0 +1,7 @@
+const apiKey = '0e783a7e49a5ff9c8f1c1660e7a30ce1';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={0e783a7e49a5ff9c8f1c1660e7a30ce1}';
+
+fetch(url, {
+    
+}).then((response) => response)
+.then((data) => console.log(data.json()))
