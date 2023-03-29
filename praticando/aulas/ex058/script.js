3a93a4901b2b7a7f7bf9b84922609777559b3460
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2023-03-29 20:13:48 -0300

    Update script.js

--- praticando/aulas/ex058/script.js
+++ praticando/aulas/ex058/script.js
@@ -9,10 +9,10 @@ const countryflag = document.querySelector('#country');
 const temperature = document.querySelector('#temperature');
 const description = document.querySelector('#description');
 const umidity = document.querySelector('#umidity');
-const wind = document.querySelector('#wind')
+const wind = document.querySelector('#wind');
 const weatherIcon = document.querySelector('#weather-icon')
 const hide = [...document.querySelectorAll('.hide')];
-const err = document.querySelector('#error-message')
+const err = document.querySelector('#error-message');
 
 const showBackground = nameCity => {
     nameCity = nameCity.replace(" ","");
@@ -22,14 +22,14 @@ const showBackground = nameCity => {
 
 button.addEventListener('click', (evt) => {
     const cityName = input.value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}&lang=pt_br`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}&lang=pt_br`;
     const requestAPI = fetch(url)
     .then(response => response.json())
     .then(data => {
         showBackground(cityName);
         hide.map((element, index) => {
             element.style.display = 'block';
-            err.style.display = 'none'
+            err.style.display = 'none';
         });
         countryflag.src = `https://flagsapi.com/${data.sys.country}/shiny/64.png`;
         city.innerHTML = cityName;
@@ -44,8 +44,8 @@ button.addEventListener('click', (evt) => {
     })
     .catch(error => {
         hide.map((element, index) => {
-            element.style.display = 'none'
-            err.style.display = 'block'
+            element.style.display = 'none';
+            err.style.display = 'block';
         });
     });
 });
