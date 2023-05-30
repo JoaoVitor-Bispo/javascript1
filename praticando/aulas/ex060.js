05f495d1ba95a871acee02a41ad6bbc6fa674183
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2023-05-30 19:29:07 -0300

    Update ex060.js

--- praticando/aulas/ex060.js
+++ praticando/aulas/ex060.js
@@ -1,16 +1,19 @@
 function chunkArrayInGroups(arr, size) {
-
+    let c = 0;
+    let index = 0;
     const newArr = [];
-
-    for(let c = 1;c <= arr.length;c++) {
-        let i = 0
-        if(c % size == 0) {
-            const teste = arr;
-            newArr.push(arr.slice(i, c));
-            i += c;
+    for(let i = 1; i <= Math.round(arr.length / size); i++) {
+        newArr.push([]);
+    };
+    while(c <= (arr.length - 1)) {
+        console.log(c)
+        newArr[index].push(arr[c])
+        if(c % size === 0) {
+            index++;
         };
+        c++;
     };
     console.log(newArr);
 };
   
-  chunkArrayInGroups(["a", "b", "c", "d"], 2);
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);
